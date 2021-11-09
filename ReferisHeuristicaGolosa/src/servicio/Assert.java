44925f3401f06cf257736f9f8bdd787c15d2416f
manuavalos-git <manuavalos-git@github.com>
2021-11-09 16:36:20 -0300

    test solver

---
+++ ReferisHeuristicaGolosa/src/servicio/Assert.java
@@ -0,0 +1,66 @@
+package servicio;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+public class Assert
+{
+	
+	public static void todosCeros(Calendario calendario)
+	{
+		boolean todosCeros=true;
+		for (Fecha fecha : calendario.copiaFechas()) {
+			for (Partido partido : fecha.getPartidos()) {
+				if(partido.getArbitro()!=0) {
+					todosCeros=todosCeros&&false;
+				}
+			}
+		}
+		assertTrue(todosCeros);
+	}
+
+	public static void sinCeros(ArrayList<Fecha> lista) {
+		boolean sinCeros=true;
+		for (Fecha fecha : lista) {
+			for (Partido partido : fecha.getPartidos()) {
+				if(partido.getArbitro()==0) {
+					sinCeros=sinCeros&&false;
+				}
+			}
+		}
+		assertTrue(sinCeros);
+	}
+
+	public static void fechaEscalonada(ArrayList<Fecha> lista) {
+		int sumaEsperada=sumatoria((lista.size()+1)/2);
+		int sumaReturn=0;
+		for (Partido partido : lista.get(0).getPartidos()) {
+			sumaReturn+=partido.getArbitro();
+		}
+		assertEquals(sumaEsperada,sumaReturn);
+	}
+	public static void fechasDiferentes(ArrayList<Fecha> lista) {
+		boolean fechasDiferentes=true;
+		for (Fecha fecha : lista) {
+			ArrayList<Integer> recorridos=new ArrayList<Integer>();
+			for (Partido partido: fecha.getPartidos()) {
+				if(!recorridos.contains(partido.getArbitro())){
+					recorridos.add(partido.getArbitro());
+				}
+			}
+			if (recorridos.size()!=(lista.size()+1)/2) {
+				fechasDiferentes=fechasDiferentes&&false;
+			}
+		}
+		assertTrue(fechasDiferentes);
+		
+	}
+	private static int sumatoria(int i) {
+		int sumatoria=0;
+		for(int j=1;j<=i;j++) {
+			sumatoria+=j;
+		}
+		return sumatoria;
+	}
+}
---
+++ ReferisHeuristicaGolosa/src/servicio/SolverArbitroTest.java
@@ -0,0 +1,57 @@
+package servicio;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class SolverArbitroTest {
+
+	@Test
+	void sinSolver() {
+		Calendario calendario=leerSerializado("C:\\Users\\alumno\\git\\referisHeuristicaGolosa\\ReferisHeuristicaGolosa\\src\\servicio\\calendarioLigaEspañola.ddr");
+	
+		Assert.todosCeros(calendario);
+	}
+	@Test
+	void sinCeros() {
+		ArrayList<Fecha> lista=listaFechas();
+		
+		Assert.sinCeros(lista);
+	}
+	@Test
+	void primerFechaEscalonada() {
+		ArrayList<Fecha> lista=listaFechas();
+		
+		Assert.fechaEscalonada(lista);
+	}
+	@Test
+	void todasDiferentes() {
+		ArrayList<Fecha> lista=listaFechas();
+		
+		Assert.fechasDiferentes(lista);
+	}
+	private ArrayList<Fecha> listaFechas() {
+		Calendario calendario=leerSerializado("C:\\Users\\alumno\\git\\referisHeuristicaGolosa\\ReferisHeuristicaGolosa\\src\\servicio\\calendarioLigaEspañola.ddr");
+		SolverArbitro solver=new SolverArbitro(calendario);
+		
+		return solver.resolver();
+	}
+	private static Calendario leerSerializado(String ubicacion) {
+		Calendario calen=new Calendario();
+		try 
+		{
+		FileInputStream fis = new FileInputStream(ubicacion);
+		ObjectInputStream in = new ObjectInputStream(fis);
+		
+		calen= (Calendario) in.readObject();
+		
+		in.close();
+		}
+		catch (Exception ex) { 
+			
+		}
+		return calen;
+	}
+}
