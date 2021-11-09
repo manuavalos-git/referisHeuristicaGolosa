a05a8775e4b64948c3e296e3363d3d79fc5d701e
manuavalos-git <manuavalos-git@github.com>
2021-11-09 17:18:47 -0300

    test equipo

---
+++ ReferisHeuristicaGolosa/src/servicio/EquipoTest.java
@@ -0,0 +1,27 @@
+package servicio;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class EquipoTest {
+
+	@Test
+	void iguales() {
+		Equipo e1=new Equipo("La Estrella");
+		Equipo e2=new Equipo("La Estrella");
+		
+		assertTrue(e1.equals(e2));
+		
+	}
+	@Test
+	void diferentes() {
+		Equipo e1=new Equipo("La Estrella");
+		Equipo e2=new Equipo("Los Paraisos");
+		
+		assertFalse(e1.equals(e2));
+		
+	}
+
+}
