b9ab9c503d66649f86a5dd1948582afd03049ef9
manuavalos-git <manuavalos-git@github.com>
2021-11-09 16:48:09 -0300

    test partido

---
+++ ReferisHeuristicaGolosa/src/servicio/PartidoTest.java
@@ -0,0 +1,30 @@
+package servicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PartidoTest {
+
+	@Test
+	void igualesAlDerecho() {
+		Partido p1=new Partido(new Equipo("Dinamita"),new Equipo("El Dinamo"));
+		Partido p2=new Partido(new Equipo("Dinamita"),new Equipo("El Dinamo"));
+		
+		assertTrue(p1.equals(p2));
+	}
+	@Test
+	void igualesAlReves() {
+		Partido p1=new Partido(new Equipo("El Dinamo"),new Equipo("Dinamita"));
+		Partido p2=new Partido(new Equipo("Dinamita"),new Equipo("El Dinamo"));
+		
+		assertTrue(p1.equals(p2));
+	}
+	@Test
+	void noIguales() {
+		Partido p1=new Partido(new Equipo("Trueno"),new Equipo("Relampago"));
+		Partido p2=new Partido(new Equipo("Trueno"),new Equipo("Rayo"));
+		
+		assertFalse(p1.equals(p2));
+	}
+}
