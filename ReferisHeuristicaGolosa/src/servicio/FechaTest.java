579be58c38e3bffd35daed7f8d1d9b1703e87f6c
manuavalos-git <manuavalos-git@github.com>
2021-11-09 17:08:13 -0300

    test fecha

---
+++ ReferisHeuristicaGolosa/src/servicio/FechaTest.java
@@ -0,0 +1,49 @@
+package servicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class FechaTest {
+
+	@Test
+	void iguales() {
+		Fecha fecha1=generarfecha1();
+		Fecha fecha2=generarfecha2();
+		
+		assertTrue(fecha1.equals(fecha2));
+	}
+	@Test
+	void  dferentes() {
+		Fecha fecha1=generarfecha1();
+		Fecha fecha2=generarfecha3();
+		
+		assertFalse(fecha1.equals(fecha2));
+	}
+
+	private Fecha generarfecha3() {
+		ArrayList<Partido> partidos=new ArrayList<Partido>();
+		partidos.add(new Partido(new Equipo("San Miguel"),new Equipo("Boca")));
+		partidos.add(new Partido(new Equipo("Racing"),new Equipo("Lanus")));
+		partidos.add(new Partido(new Equipo("santa Cruz"),new Equipo("Chacarita")));
+		return new Fecha(partidos);
+	}
+	private Fecha generarfecha2() {
+		ArrayList<Partido> partidos=new ArrayList<Partido>();
+		partidos.add(new Partido(new Equipo("Boca"),new Equipo("River")));
+		partidos.add(new Partido(new Equipo("Lanus"),new Equipo("Racing")));
+		partidos.add(new Partido(new Equipo("santa Cruz"),new Equipo("Chacarita")));
+		return new Fecha(partidos);
+	}
+
+	private Fecha generarfecha1() {
+		ArrayList<Partido> partidos=new ArrayList<Partido>();
+		partidos.add(new Partido(new Equipo("River"),new Equipo("Boca")));
+		partidos.add(new Partido(new Equipo("Racing"),new Equipo("Lanus")));
+		partidos.add(new Partido(new Equipo("santa Cruz"),new Equipo("Chacarita")));
+		return new Fecha(partidos);
+	}
+
+}
